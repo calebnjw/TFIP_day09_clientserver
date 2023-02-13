16de81fa562ee4d86e6f9d50bd0d7d9be14194eb
Caleb <5322420+calebnjw@users.noreply.github.com>
2023-02-13 10:58:28 +0800

    basic number guessing game

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,9 +1,33 @@
 package app;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App {
+  private App() {
+  }
 
+  public static void main(String[] args) {
+    Random random = new Random();
+    Integer randomNum = random.nextInt(100);
+    Integer guess = 0;
+
+    Scanner scanner = new Scanner(System.in);
+    while (guess != randomNum) {
+      guess = scanner.nextInt();
+
+      if (guess < randomNum) {
+        System.out.println("Guess a bigger number.");
+      } else if (guess > randomNum) {
+        System.out.println("Guess a smaller number.");
+      } else {
+        System.out.println("You got it!");
+        scanner.close();
+      }
+    }
+  }
 }
